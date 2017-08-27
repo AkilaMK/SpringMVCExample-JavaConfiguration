d2883b0d12eac9600f579cf36700b94d382db38c
akila <akilamahesha@gmail.com>
2017-08-27 11:39:55 +0530

    spring mvc example project with java configuration

---
+++ README.txt
@@ -0,0 +1,5 @@
+http://websystique.com/springmvc/spring-4-mvc-helloworld-tutorial-annotation-javaconfig-full-example/
+
+mvn clean install
+copy to tomcat/webapps/ directory
+./catalina.sh run
---
+++ src/main/java/com/akila/springmvc/example/configuration/HelloWorldInitializer.java
@@ -0,0 +1,29 @@
+package com.akila.springmvc.example.configuration;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * Created by akila on 8/27/17.
+ */
+
+public class HelloWorldInitializer implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext container) throws ServletException {
+
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(HelloWorldConfiguration.class);
+        ctx.setServletContext(container);
+
+        ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
+
+}
